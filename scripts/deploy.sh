90da79bbcc3df3e753bd75419fcd867144f786c8
Lucy Oh <56earls@gmail.com>
2024-01-09 03:46:09 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,12 @@
 REPOSITORY=/home/ubuntu/bobmate
 cd $REPOSITORY
 
-APP_NAME=unibond
+APP_NAME=bobmate
+
+APP_LOG=/home/ubuntu/bobmate/application.log
+ERROR_LOG=/home/ubuntu/bobmate/application.log
+DEPLOY_LOG=/home/ubuntu/bobmate/application.log
+
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
@@ -19,4 +24,4 @@ else
 fi
 
 echo "> Deploy - $JAR_PATH "
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Duser.timezone=Asia/Seoul $JAR_PATH --logging.level.org.hibernate.SQL=DEBUG > $APP_LOG 2>$ERROR_LOG &
