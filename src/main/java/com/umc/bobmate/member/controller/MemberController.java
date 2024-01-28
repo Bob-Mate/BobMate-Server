31ebd5423bf4ea6f4a1c2a3a36b50c8fe647ef07
5jisoo <56earls@gmail.com>
2024-01-29 01:39:56 +0900

    #6 Feat: 한 줄 코멘트 API

--- src/main/java/com/umc/bobmate/member/controller/MemberController.java
+++ src/main/java/com/umc/bobmate/member/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.umc.bobmate.member.controller;
 
 import com.umc.bobmate.global.apiPayload.ApiResponse;
+import com.umc.bobmate.member.dto.request.CommentUploadRequest;
 import com.umc.bobmate.member.dto.request.PreferenceUploadRequest;
+import com.umc.bobmate.member.dto.response.CommentResponse;
 import com.umc.bobmate.member.dto.response.PreferenceResponse;
 import com.umc.bobmate.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -24,5 +26,16 @@ public class MemberController {
     @GetMapping("/api/v1/members/preference")
     public ApiResponse<PreferenceResponse> getContentPreference() {
         return ApiResponse.onSuccess(memberService.getContentPreference());
+    }
+
+    @PostMapping("/api/v1/members/comment")
+    public ApiResponse<Void> saveComment(@RequestBody final CommentUploadRequest uploadRequest) {
+        memberService.saveComment(uploadRequest);
+        return ApiResponse.onSuccess();
+    }
+
+    @GetMapping("/api/v1/members/comment")
+    public ApiResponse<CommentResponse> getComment() {
+        return ApiResponse.onSuccess(memberService.getComment());
     }
 }
--- src/main/java/com/umc/bobmate/member/service/MemberService.java
+++ src/main/java/com/umc/bobmate/member/service/MemberService.java
@@ -1,11 +1,15 @@
 package com.umc.bobmate.member.service;
 
+import com.umc.bobmate.comment.domain.Comment;
+import com.umc.bobmate.comment.domain.repository.CommentRepository;
 import com.umc.bobmate.global.apiPayload.exception.GeneralException;
 import com.umc.bobmate.login.jwt.util.AuthTokensGenerator;
 import com.umc.bobmate.member.domain.Member;
 import com.umc.bobmate.member.domain.repository.CustomMemberRepository;
 import com.umc.bobmate.member.domain.repository.MemberRepository;
+import com.umc.bobmate.member.dto.request.CommentUploadRequest;
 import com.umc.bobmate.member.dto.request.PreferenceUploadRequest;
+import com.umc.bobmate.member.dto.response.CommentResponse;
 import com.umc.bobmate.member.dto.response.PreferenceResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +25,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final CustomMemberRepository customMemberRepository;
     private final AuthTokensGenerator authTokensGenerator;
+    private final CommentRepository commentRepository;
 
     public Member findById(Long id) {
         return memberRepository.findById(id).orElseThrow(() -> new GeneralException(INVALID_MEMBER_ID));
@@ -39,5 +44,25 @@ public class MemberService {
                 loginMember.getName(),
                 preferenceList
         );
+    }
+
+    @Transactional
+    public void saveComment(CommentUploadRequest uploadRequest) {
+        final Member loginMember = authTokensGenerator.getLoginMember();
+
+        final Comment comment = Comment.builder()
+                .member(loginMember)
+                .genre(uploadRequest.getGenre())
+                .food(uploadRequest.getFood())
+                .emotion(uploadRequest.getEmotion())
+                .build();
+
+        commentRepository.save(comment);
+    }
+
+    public CommentResponse getComment() {
+        final Member loginMember = authTokensGenerator.getLoginMember();
+        final Comment comment = commentRepository.findFirstByMemberOrderByCreatedDateDesc(loginMember);
+        return new CommentResponse(comment);
     }
 }
