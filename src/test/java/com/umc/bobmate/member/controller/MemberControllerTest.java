fe6db41b60e99a4d34bf59f5e8108304b84d49c3
5jisoo <56earls@gmail.com>
2024-01-29 02:01:59 +0900

    Chore: fix test files

--- src/test/java/com/umc/bobmate/member/controller/MemberControllerTest.java
+++ src/test/java/com/umc/bobmate/member/controller/MemberControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -50,7 +51,8 @@ class MemberControllerTest {
 
     ObjectMapper objectMapper = new ObjectMapper();
 
-    String accessToken = "";
+    @Value("${test.accessKey}")
+    String accessToken;
 
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
