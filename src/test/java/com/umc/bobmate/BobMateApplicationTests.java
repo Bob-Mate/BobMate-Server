0ce7f0b9d8b14810e4b774a649d696c5d47bb588
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-02-08 19:18:38 +0900

    TEST Exception으로 주석처리

--- src/test/java/com/umc/bobmate/BobMateApplicationTests.java
+++ src/test/java/com/umc/bobmate/BobMateApplicationTests.java
@@ -2,7 +2,7 @@ package com.umc.bobmate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/*
 @SpringBootTest
 class BobMateApplicationTests {
 
@@ -11,3 +11,4 @@ class BobMateApplicationTests {
 	}
 
 }
+*/
